aedc071f66f67f0040b59d1abbf7afa9e84872f4
Katrin Haslinger <20150088@htbla-perg.onmicrosoft.com>
2019-10-16 17:06:46 +0200

    add homework with bug

---
+++ TrippinUser/TrippinUser/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TrippinUser
+{
+	class Program
+	{
+		public static HttpClient client = new HttpClient() { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/(S(4ry5xpsxhfj33xna2sghsnlw))/") };
+		static string filename = "users.json";
+
+		static async Task Main(string[] args)
+		{
+
+			// read users from file
+			var fileText = await ReadFile(filename);
+			var fileUsers = JsonSerializer.Deserialize<List<FileUser>>(fileText);
+
+			//add missing users
+			AddMissingUsers(fileUsers);
+
+		}
+
+		/// <summary>
+		/// Reads a json file
+		/// </summary>
+		/// <param name="filename"></param>
+		/// <returns>A string with the file content</returns>
+		public static async Task<string> ReadFile(string filename)
+		{
+
+			Console.WriteLine("readFile");
+			var filetext = await File.ReadAllTextAsync(filename);
+
+			return filetext;
+
+		}
+
+		/// <summary>
+		/// Checks if a user from the .json- file is already in the API, if not, the user is added
+		/// </summary>
+		/// <param name="fileUsers"></param>
+		public async static void AddMissingUsers(List<FileUser> fileUsers)
+		{
+			Console.WriteLine("fileUser size:" + fileUsers.Count);
+			Console.WriteLine(fileUsers[1].FirstName);
+
+			foreach (FileUser user in fileUsers)
+			{
+				Console.WriteLine("in Loop");
+
+				var userResponse = await client.GetAsync("People('" + user.UserName + "')");
+
+				Console.WriteLine("in Loop 2");
+
+				if (!userResponse.IsSuccessStatusCode)
+				{
+					Console.WriteLine("in if");
+
+					Console.WriteLine("User added: " + user.UserName);
+					HttpResponseMessage response = await client.PostAsync("People", new StringContent(JsonSerializer.Serialize(new
+					{
+						user.UserName,
+						user.FirstName,
+						user.LastName,
+						Email = new List<string> { user.Emails },
+						AddressInfo = new List<object>{
+									new
+									{
+										user.Address,
+										City = new
+										{
+											Name = user.CityName,
+											CountryRegion = user.Country,
+											Region = user.Country
+										}
+									}
+					}
+
+					}), Encoding.UTF8, "application/json"));
+					
+				}
+				else
+				{
+					Console.WriteLine("in else");
+					Console.WriteLine("User already exists: " + user.UserName);
+				}
+
+				Console.WriteLine("End of code");
+			}
+
+
+		}
+	}
+}
+
+
---
+++ TrippinUser/TrippinUser/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace TrippinUser
+{
+
+	public class FileUser
+	{
+	
+		public string UserName { get; set; }
+
+		
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		
+		public string Emails { get; set; }
+
+		
+		public string Address { get; set; }
+
+		
+		public string CityName { get; set; }
+
+		
+		public string Country { get; set; }
+
+	
+	}
+
+	public class FileUsers
+	{
+		
+		public List<FileUser> fileUsers { get; set; }
+	}
+
+}
